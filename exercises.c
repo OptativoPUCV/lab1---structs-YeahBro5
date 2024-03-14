1ea7e440bafa90f888983da5ec58d1d1f11329eb
Your Name <you@example.com>
2024-03-14 21:16:51 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -130,8 +130,6 @@ void mergeSortedArrays(int arr1[], int size1, int arr2[], int size2,
   
 }
 
-free(result);
-
 /*
 Ejercicio 5: Comprobación de Ordenación
 Descripción: Escribe una función que tome un arreglo y su tamaño,
